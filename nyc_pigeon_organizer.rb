42ef58cad68964c4220d93c8af041b54a6c8a856
Rubens Fernandes <rubens.fernandes97@gmail.com>
2020-04-28 14:16:03 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  all_names = {}
+  
+    data.each do |key, value|
+      value.each do |key2, value2|
+        value2.each do |name|
+          if !all_names[name]
+            all_names[name] = {}
+          end
+          if !all_names[name][key]
+            all_names[name][key] = []
+          end
+    
+            all_names[name][key].push(key2.to_s)
+          
+        
+        end
+      end
+    end
+    all_names
 end
